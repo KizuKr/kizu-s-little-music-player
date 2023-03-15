42057cdb2af39cc5409fefdc5708434afe9750c3
KizuKr <127852028+KizuKr@users.noreply.github.com>
2023-03-15 16:48:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -80,6 +80,66 @@ const music_list = [
         name : 'Haraad Suuna',
         artist : 'Enkhe x Erkhem',
         music : 'Enkhe x Erkhem _ Haraad suuna _ lyric _Хараад сууна_.mp3'
+    },
+    {
+        img : 'hsvr.jpg',
+        name : 'Hiisver',
+        artist : '9SUULT',
+        music : 'Hiisver.mp3'
+    },
+    {
+        img : 'dur.jpg',
+        name : 'Durlaagui Baisan Bol',
+        artist : 'Gvne & Xmo',
+        music : 'durl.mp3'
+    },
+    {
+        img : 'uhlee.jpg',
+        name : 'Uhlee Huleene',
+        artist : 'Indigo Lemox & Xmo & Enkhe X Erkhem',
+        music : 'Indigo Lemon x Xmo x Enkhe “Үхлээ хүлээнэ” (uhlee huleene) lyrics.mp3'
+    },
+    {
+        img : 'hamgiin.jpg',
+        name : 'Хамгийн сайхан нь',
+        artist : 'OPOZIT',
+        music : 'OPOZIT - HAMGIIN SAIHAN N OST (LYRIC VIDEO).mp3'
+    },
+    {
+        img : 'down.jpg',
+        name : 'Down',
+        artist : 'Seto',
+        music : 'SETO -  DOWN.mp3'
+    },
+    {
+        img : 'monstar.jpg',
+        name : 'Huuhdeeree Boy',
+        artist : 'Vande',
+        music : 'Vande - Huuhdeeree Boy _Monstar Live_.mp3'
+    },
+    {
+        img : 'haraal.jpg',
+        name : 'Nadad Itgeerei',
+        artist : 'Vande ft. FLA',
+        music : 'Vande - Nadad Itgeerei ft. FLA (Official Music Video).mp3'
+    },
+    {
+        img : 'shunu.jpg',
+        name : 'shunujin',
+        artist : 'SASH',
+        music : 'sash. - shunujin.mp3'
+    },
+    {
+        img : 'side.jpg',
+        name : 'SIDE EFFECT',
+        artist : 'Morningstar ft. THE C',
+        music : 'Morningstar - SIDE EFFECTS ft The C (Official Music Video).mp3'
+    },
+    {
+        img : 'zone.jpg',
+        name : 'ZONE',
+        artist : 'OG.MOB ft. Vandebo',
+        music : 'O.G MOB x Vandebo - Zone (Official Music Video).mp3'
     }
 ];
 
